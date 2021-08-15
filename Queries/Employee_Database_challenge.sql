af585a28535e1ee0fb25abc71aec1abeff5661e3
Esther Utomakili <okeuto@gmail.com>
2021-08-15 10:28:10 -0400

    Query Update

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,18 +1,48 @@
-SELECT emp_no, first_name, last_name
-FROM employees
-SELECT title, from_date, to_date
-FROM titles
-INTO
+-- D1
 
-
-SELECT e.emp_no
-	e.first_name
-	e.last_name
-	t.title
-	t.from_date
-	t.to_date
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    t.title,
+    t.from_date,
+    t.to_date
 INTO retirement_titles
 FROM employees AS e
 INNER JOIN titles AS t
 ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31');
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+SELECT COUNT(title) AS title_count, title
+INTO  retiring_titles
+FROM unique_titles
+GROUP BY title
+
+DROP TABLE mentorship_eligibilty
+
+-- D2
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-1-01'AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibilty
